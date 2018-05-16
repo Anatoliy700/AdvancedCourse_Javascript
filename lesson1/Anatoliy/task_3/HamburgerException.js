e752dec3b63b7aaec93a01b5717ea9a2043090c8
Anatoliy <anatoliy.rus@gmail.com>
2018-05-16 22:41:40 +0300

    Задание 3 (начал верстку)

--- lesson1/Anatoliy/task_3/HamburgerException.js
+++ lesson1/Anatoliy/task_3/HamburgerException.js
@@ -3,10 +3,10 @@
 /**
  * Представляет информацию об ошибке в ходе работы с гамбургером.
  * Подробности хранятся в свойстве message.
- * @constructor
  */
 class HamburgerException extends Error {
 
+  //TODO: доделать JSDoc
   constructor(message) {
     super(message);
     // this.message = message;
---
+++ lesson1/Anatoliy/task_3/contriller.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const settings = {
+
+};
+const controller = {
+settings,
+
+
+};
