f78ff475293d05cabab73c16985e137acb04b9e5
Anatoliy <anatoliy.rus@gmail.com>
2018-05-16 15:29:51 +0300

    Закончил задание 2

--- lesson1/Anatoliy/Menu.js
+++ lesson1/Anatoliy/Menu.js
@@ -1,35 +1,33 @@
-class Menu
-{
-    //Конструктор - метод, который вызывается до создания объекта
-    constructor(id, className, items)
-    {
-        this.id = id;
-        this.className = className;
-        this.items = items;
-    }
-    render()
-    {
-        let result = `<ul class="${this.className}" id="${this.id}">`;
+class Menu {
+  //Конструктор - метод, который вызывается до создания объекта
+  constructor(id, className, items) {
+    this.id = id;
+    this.className = className;
+    this.items = items;
+  }
 
-        for(let i = 0; i < this.items.length; i++)
-        {
-            if (this.items[i] instanceof MenuItem) {
-                result += this.items[i].renderItem(); //Если это пункт меню
-            }
-            if (this.items[i] instanceof Submenu)
-            {
-                //Необходимо сделать обертку
-                result += this.items[i].renderItem();
-            }
-        }
+  render() {
+    let result = `<ul class="${this.className}" id="${this.id}">`;
 
-        result += '</ul>';
-        return result;
+    for (let i = 0; i < this.items.length; i++) {
+      if (this.items[i] instanceof MenuItem) {
+        result += this.items[i].renderItem(); //Если это пункт меню
+      }
+      if (this.items[i] instanceof Submenu) {
+        result += this.items[i].render();
+        // //Необходимо сделать обертку
+        // result += this.items[i].renderItem();
+      }
     }
-    remove(){
-        let menuElem = document.getElementById(this.id);
-        menuElem.parentElement.removeChild(menuElem);
-    }
+
+    result += '</ul>';
+    return result;
+  }
+
+  remove() {
+    let menuElem = document.getElementById(this.id);
+    menuElem.parentElement.removeChild(menuElem);
+  }
 }
 
 
--- lesson1/Anatoliy/Submenu.js
+++ lesson1/Anatoliy/Submenu.js
@@ -1,4 +1,18 @@
-class Submenu extends Menu
-{
+class Submenu extends Menu {
+  constructor(id, className, items, title) {
+    super(id, className, items);
+    this.title = title;
+  }
 
+  render() {
+    let result = `<li><a href="#">${this.title}</a>`;
+    result += super.render();
+    result += '</li>';
+    return result;
+  }
+
+  remove() {
+    let parentElem = document.getElementById(this.id).parentElement;
+    parentElem.parentElement.removeChild(parentElem);
+  }
 }
