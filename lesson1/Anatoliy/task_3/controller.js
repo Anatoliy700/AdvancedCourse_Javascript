999731a5c34bd8f26a9d3749eafb2dce819482fa
Anatoliy <anatoliy.rus@gmail.com>
2018-05-18 20:03:42 +0300

    Задание 3 (поправлен метод)

--- lesson1/Anatoliy/task_3/controller.js
+++ lesson1/Anatoliy/task_3/controller.js
@@ -1,5 +1,21 @@
 "use strict";
 
+/**
+ * Объект с ID используемых HTML элементов.
+ *
+ * @property {string} idElementSize id HTML элемента для вывода размера.
+ * @property {string} idElementStuffing id HTML элемента для вывода добавленных начинок.
+ * @property {string} idElementTopping id HTML элемента для вывода добавленных добавок.
+ * @property {string} idElementPrice id HTML элемента для вывода стоимости.
+ * @property {string} idElementCalories id HTML элемента для вывода калорий.
+ * @property {string} idElementForm id HTML элемента формы выбора данных.
+ * @property {string} idElementOrderBtn id HTML элемента кнопки обработки выбранных данных.
+ * @property {string} idElementToppingMayoAddBtn id HTML элемента кнопки добавления добавки.
+ * @property {string} idElementToppingMayoDelBtn id HTML элемента кнопки удаления добавки.
+ * @property {string} idElementToppingSpiceAddBtn id HTML элемента кнопки добавления добавки.
+ * @property {string} idElementToppingSpiceDelBtn id HTML элемента кнопки удаления добавки.
+ * @property {string} idElementToppingWrap id HTML элемента обертки блока с добавками.
+ */
 const settings = {
   idElementSize: 'size',
   idElementStuffing: 'stuffing',
@@ -14,6 +30,11 @@ const settings = {
   idElementToppingSpiceDelBtn: 'topping_spice_del',
   idElementToppingWrap: 'topping_wrap',
 };
+
+/**
+ *
+ * @type {{settings: {idElementSize: string, idElementStuffing: string, idElementTopping: string, idElementPrice: string, idElementCalories: string, idElementForm: string, idElementOrderBtn: string, idElementToppingMayoAddBtn: string, idElementToppingMayoDelBtn: string, idElementToppingSpiceAddBtn: string, idElementToppingSpiceDelBtn: string, idElementToppingWrap: string}, sizeElem: null, stuffingElem: null, toppingElem: null, priceElem: null, caloriesElem: null, formElem: null, orderBtnElem: null, toppingWrap: null, hamburger: null, init(): void, toPlaceAnOrder(*): void, addOrDelTopping(*): void, render(*, *=): void}}
+ */
 const controller = {
   settings,
   sizeElem: null,
@@ -26,6 +47,9 @@ const controller = {
   toppingWrap: null,
   hamburger: null,
 
+  /**
+   *
+   */
   init() {
     this.sizeElem = document.getElementById(this.settings.idElementSize);
     this.stuffingElem = document.getElementById(this.settings.idElementStuffing);
@@ -40,10 +64,13 @@ const controller = {
     // console.log(inputArr);
 
     this.orderBtnElem.addEventListener('click', () => this.toPlaceAnOrder(this.formElem));
-    this.toppingWrap.addEventListener('click', () => this.addOrDelTopping(event));
-
+    this.toppingWrap.addEventListener('click', event => this.addOrDelTopping(event));
   },
 
+  /**
+   *
+   * @param formElem
+   */
   toPlaceAnOrder(formElem) {
     const inputArr = formElem.querySelectorAll('input:checked');
 
@@ -62,15 +89,13 @@ const controller = {
     this.render(this.toppingElem, this.hamburger.getToppings());
     this.render(this.priceElem, this.hamburger.calculatePrice());
     this.render(this.caloriesElem, this.hamburger.calculateCalories());
-
-
-    // console.log(this.hamburger);
-
   },
 
+  /**
+   *
+   * @param event
+   */
   addOrDelTopping(event) {
-
-    // console.log(event);
 
     const elem = event.target;
     if (elem.tagName === 'BUTTON' && this.hamburger !== null) {
@@ -86,7 +111,11 @@ const controller = {
     }
   },
 
-
+  /**
+   *
+   * @param elem
+   * @param data
+   */
   render(elem, data) {
     if (data !== undefined) elem.innerHTML = data;
   },
