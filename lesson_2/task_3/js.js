eb295004659e50faafe8d81efcd5b31159e9dadd
Anatoliy <anatoliy.rus@gmail.com>
2018-05-19 17:43:21 +0300

    lesson_2 Задание 3 (добавил JSDoc и картинки)

--- lesson_2/task_3/js.js
+++ lesson_2/task_3/js.js
@@ -1,8 +1,13 @@
 "use strict";
 
-
+/**
+ * Объект для вывода и работы с фотогалереей.
+ */
 const photoGallery = {
 
+  /**
+   * Отправляет AJAX запрос, получает JSON с объектами ссылок на картинки и запускает отрисовку галереи.
+   */
   init() {
     const request = new XMLHttpRequest();
     request.open('GET', 'img_src.json');
@@ -19,6 +24,11 @@ const photoGallery = {
     }(this);
   },
 
+  /**
+   * Отрисовывает галерею и навешивает собыитие.
+   * @param {object[]} srcArr массив объектов с сылками на картинки.
+   * @param {string} idWrap id HTML элемента, в котором содираем галерею.
+   */
   renderImg(srcArr, idWrap) {
     const elementParent = document.getElementById(idWrap);
     for (let obj of srcArr) {
@@ -30,11 +40,13 @@ const photoGallery = {
     elementParent.addEventListener('click', event => this.clickHandler(event));
   },
 
+  /**
+   * Обрабатывает событие click по обертке и дочерним элементам, выбирая тока click по IMG и обрабатывае его.
+   * Открывает картинку по которой был click на весь экран.
+   * @param {object} event объект собития click который произошел.
+   */
   clickHandler(event) {
     if (event.target.tagName !== "IMG") return;
-
-    // event.target.src = event.target.dataset.maxSrc;
-
     const div = document.createElement('div');
     div.classList.add('openImageWrap');
     const img = document.createElement('img');
