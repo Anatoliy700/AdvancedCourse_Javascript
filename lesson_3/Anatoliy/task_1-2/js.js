4d5136fc92aa342d5d180fa731dceadd1f480693
Anatoliy <anatoliy.rus@gmail.com>
2018-05-23 21:01:23 +0300

    lesson_3 (задание 1-2 вроде закончил))

---
+++ lesson_3/Anatoliy/task_1-2/js.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const converterString = {
+  container: null,
+  input: null,
+  button: null,
+  out: null,
+
+  init() {
+    this.container = document.querySelector('body');
+    this.input = document.createElement('textarea');
+    //width: 330px; height: 64px;"
+    this.input.style.width = '330px';
+    this.input.style.height = '64px';
+    this.input.value = `'Lorem ipsum'dolor sit 'amet'.`;
+    this.button = document.createElement('button');
+    this.button.innerText = 'Конвертировать';
+    this.out = document.createElement('p');
+    this.container.appendChild(this.input);
+    this.container.appendChild(this.button);
+    this.container.appendChild(this.out);
+
+    this.button.addEventListener('click', () => this.btnClickHandler());
+  },
+
+  btnClickHandler() {
+    let data = this.input.value;
+    let newData = data.replace(/'/g, '"');
+    newData = newData.replace(/\b"\b/g, `'`);
+    this.out.innerText =newData;
+  },
+
+
+};
+
+window.onload = () => converterString.init();
