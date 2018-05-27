15eddaf7ab88da29b5a0e6fb4a484d59180d428c
Anatoliy <anatoliy.rus@gmail.com>
2018-05-27 20:02:13 +0300

    lesson_4 (задание 1 закончил)

---
+++ lesson_4/Anatoliy/task_1/js.js
@@ -0,0 +1,10 @@
+"use strict";
+
+$(document).ready(function () {
+  $(`.tabs`).on('click', 'li', function () {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    $(this).parents('.wrap').children('.wrap-content').eq(0).children('.content')
+      .removeClass('active').eq($(this).index()).addClass('active');
+  })
+});
