9ed105837233c69ea911c62382194b2bb901a4d3
Anatoliy <anatoliy.rus@gmail.com>
2018-05-27 20:04:50 +0300

    lesson_4 (задание 1 поправил кавычки)

--- lesson_4/Anatoliy/task_1/js.js
+++ lesson_4/Anatoliy/task_1/js.js
@@ -1,7 +1,7 @@
 "use strict";
 
 $(document).ready(function () {
-  $(`.tabs`).on('click', 'li', function () {
+  $('.tabs').on('click', 'li', function () {
     $(this).addClass('active');
     $(this).siblings().removeClass('active');
     $(this).parents('.wrap').children('.wrap-content').eq(0).children('.content')
