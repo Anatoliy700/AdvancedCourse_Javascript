cd21a8cd1cc756b45c602dab1f21a940dfc5d700
Anatoliy <anatoliy.rus@gmail.com>
2018-05-31 20:55:37 +0300

    lesson_5 (задание 1 логика работает)

--- lesson_5/Anatoliy/task_2/js/Comment.js
+++ lesson_5/Anatoliy/task_2/js/Comment.js
@@ -1,8 +1,8 @@
 class Comment {
-  constructor(userId, message, commentId) {
-    this.id_user = userId;
+  constructor(commentId, message) {
+    // this.id_user = userId;
     this.text = message;
-    this.commentId = commentId;
+    this.id_comment = commentId;
     this.classContainer = 'comment-item';
     this.classMessage = 'comment-item_message';
     this.classDelBtn = 'comment-item_del-btn';
@@ -12,7 +12,8 @@ class Comment {
   render($jQueryElement) {
     const $containerComment = $('<div />', {
       class: this.classContainer,
-      'data-id': this.commentId,
+      'data-id': this.id_comment,
+      'data-approved': false,
     });
 
     const $messageComment = $('<p />', {
@@ -22,13 +23,15 @@ class Comment {
 
     const $delBtnComment = $('<button />', {
       class: this.classDelBtn,
-      'data-id': this.commentId,
+      'data-id': this.id_comment,
+      'data-type': 'del',
       text: 'Удалить',
     });
 
     const $approveBtnComment = $('<button />', {
       class: this.classApprovelBtn,
-      'data-id': this.commentId,
+      'data-id': this.id_comment,
+      'data-type': 'app',
       text: 'Одобрить',
     });
 
